8258a93f439aaa08ce3dafa4b128f921b1f4a11c
Viktor Kuzenkov <v.kuzenkov@fintechru.org>
2021-03-17 03:26:31 +0300

    Add context timeout

--- cmd/crlc/main.go
+++ cmd/crlc/main.go
@@ -10,6 +10,7 @@ import (
 	"net/url"
 	"os"
 	"strings"
+	"time"
 )
 
 //Ссылка на TSL https://e-trust.gosuslugi.ru/app/scc/portal/api/v1/portal/ca/getxml
@@ -25,7 +26,7 @@ func main() {
 
 	r := gin.New()
 
-	r.GET("/crl/:keyId", ReverseProxy(t))
+	r.GET("/crl/:keyId", ReverseProxy(t, logger))
 	r.GET("/debug", func(c *gin.Context) {
 		c.JSON(http.StatusOK, t.GetCDPMap())
 	})
@@ -34,14 +35,21 @@ func main() {
 		c.String(http.StatusMethodNotAllowed, "Method not allowed")
 	})
 
-	r.Run(":8080")
+	s := &http.Server{
+		Addr:         ":8080",
+		Handler:      r,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+	_ = s.ListenAndServe()
 }
 
-func ReverseProxy(t *tsl.Tsl) gin.HandlerFunc {
+func ReverseProxy(t *tsl.Tsl, logger *log.Logger) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		keyId := c.Param("keyId")
 		targetUrl := t.GetCDPMap()[strings.ToLower(keyId)]
 		u, _ := url.Parse(targetUrl[0])
+		logger.Printf("Redirecting to %s for keyId %s", u, keyId)
 		director := func(req *http.Request) {
 			req.URL = u
 			req.Host = u.Host
